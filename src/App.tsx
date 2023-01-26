266a7fdf2a2c163e029dc3c4b32f439af214723f
Ramiro Alvarez <ramiro.al.alvarez@gmail.com>
2023-01-26 11:43:39 -0300

    start

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Typography, ThemeProvider, createTheme } from "@mui/material";
+import "@fontsource/open-sans"; 
+import { Header } from './components/Header';
 
 function App() {
+
+  const theme = createTheme({
+    typography: {
+      fontFamily: [
+        "'Open Sans'",
+
+
+      ].join(","),
+    },
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ThemeProvider theme={theme} >
+        <Header />
+      </ThemeProvider>
+    </>
   );
 }
 
---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+import { Typography } from "@mui/material"
+
+
+export const Header = () => {
+  return (
+    <div>Header</div>
+  )
+}
